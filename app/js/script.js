3f451384010a1f866a95fed5d5ffc727bc159c44
Mohd Rejoan <wearecrx@gmail.com>
2021-07-27 02:09:23 +0600

    animation and form change function added

--- app/js/script.js
+++ app/js/script.js
@@ -1,51 +1,162 @@
-import '../scss/style.scss';
-import { gsap } from 'gsap';
+// import files
+import "../scss/style.scss";
+import { gsap } from "gsap";
 
-
-
-let getLink = document.querySelectorAll('button');
-
-getLink.forEach((item, index) => {
-    item.addEventListener('click', (link) => {
-        link.preventDefault()
-    })
-})
-document.querySelectorAll('a').forEach((item, index) => {
-    item.addEventListener('click', (link) => {
-        link.preventDefault()
-    })
-})
-
-
-// sign in 
-const signUp = document.querySelector("#sign_up_btn");
-const targetAnimation = gsap.utils.selector("#animation svg path");
-
-// signUp.addEventListener('click', () => {
-//     gsap.fromTo("#an_svg #circle1_left", {
-//         transformOrigin: '75% 40%',
-//         rotate: 125,
-//     });
-// })
-
-gsap.to("#circle1_left", {
-    transformOrigin: "75% 40%",
-    rotate: 125,
-    duration: 1,
-    delay: 0.5
+// disable click behave
+document.querySelectorAll("button").forEach((item, index) => {
+    item.addEventListener("click", (link) => {
+        link.preventDefault();
+    });
+});
+document.querySelectorAll("a").forEach((item, index) => {
+    item.addEventListener("click", (link) => {
+        link.preventDefault();
+    });
 });
 
-console.log(gsap.utils.toArray("#first__circle half_circle"));
+// animation setting
 
+const timeline = gsap.timeline();
+gsap.defaults({
+    duration: 1.2,
+    transformOrigin: "center",
+});
+// sign in
+const registerBtn = document.querySelector("#sign_up_btn");
+const logIn = document.querySelector("#sign_in_btn");
 
-// sign up 
+// trigger on click
+registerBtn.addEventListener("click", () => {
+    // form animation section
+    gsap.timeline()
+        .to("#sign__in", {
+            opacity: 0,
+            zIndex: 0,
+            duration: 0.35,
+        })
+        .to("#sign__in", {
+            display: "none",
+            duration: 0
+        })
+        .to("#sign__up", {
+            opacity: 1,
+            zIndex: 500,
+            duration: 0.35,
+            display: 'block'
+        });
 
+    // animation section
+    timeline
+        .to("#circle1_left", {
+            transformOrigin: "80% 35%",
+            rotate: 122,
+            x: -10 + "%",
+            y: 10 + "%",
+            fill: "#e63946",
+            duration: 1,
+            delay: 0.3,
+            animationPlayState: true
+        })
+        .to(
+            "#circle1_right",
+            {
+                rotate: 40,
+                x: 307,
+                y: -703.5,
+                fill: "#e63946",
+            },
+            "<"
+        )
+        .to(
+            "#circle2__left",
+            {
+                rotate: 60,
+                fill: "#185ADB",
+            },
+            "<"
+        )
+        .to(
+            "#circle2__right",
+            {
+                rotate: 213.5,
+                x: 378,
+                y: -358,
+                fill: "#185ADB",
+            },
+            "<"
+        )
+        .to(
+            "#portion_1",
+            {
+                rotate: 140,
+                opacity: 0,
+            },
+            "<"
+        )
+        .to(
+            "#portion_2",
+            {
+                rotate: 52,
+                x: 178,
+                y: 420,
+                opacity: 0,
+            },
+            "<"
+        )
+        .to(
+            "#portion_3",
+            {
+                rotate: 43,
+                x: 558,
+                y: 645,
+                opacity: 0,
+            },
+            "<"
+        )
+        .to(
+            "#portion_4",
+            {
+                rotate: 62,
+                x: 430,
+                y: 100,
+                opacity: 0,
+            },
+            "<"
+        )
+        .to(
+            "#first__circle",
+            {
+                x: -265,
+            },
+            "<"
+        )
+        .to(
+            "#secound__circle",
+            {
+                x: -200,
+                y: 190,
+            },
+            "<"
+        );
+});
 
+// sign in animation
+logIn.addEventListener("click", () => {
+    // form animation 
+    gsap.timeline().to('#sign__up', {
+        opacity: 0,
+        zIndex: 0,
+        duration: 0.5,
+    }).to("#sign__up", {
+        display: 'none',
+        duration: 0.1
+    }).to("#sign__in", {
+        opacity: 1,
+        zIndex: 500,
+        display: 'block',
+        duration: 0.5
+    })
 
-
-
-
-
-
-
-
+    // gsap timeline 
+    timeline.reverse();
+});
--- dist/index.371de241.js
+++ dist/index.371de241.js
@@ -428,37 +428,113 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"1oPsV":[function(require,module,exports) {
-require('../scss/style.scss');
-var _gsap = require('gsap');
-let getLink = document.querySelectorAll('button');
-getLink.forEach((item, index) => {
-  item.addEventListener('click', link => {
+require("../scss/style.scss");
+var _gsap = require("gsap");
+// disable click behave
+document.querySelectorAll("button").forEach((item, index) => {
+  item.addEventListener("click", link => {
     link.preventDefault();
   });
 });
-document.querySelectorAll('a').forEach((item, index) => {
-  item.addEventListener('click', link => {
+document.querySelectorAll("a").forEach((item, index) => {
+  item.addEventListener("click", link => {
     link.preventDefault();
   });
+});
+// animation setting
+const timeline = _gsap.gsap.timeline();
+_gsap.gsap.defaults({
+  duration: 1.2,
+  transformOrigin: "center"
 });
 // sign in
-const signUp = document.querySelector("#sign_up_btn");
-const targetAnimation = _gsap.gsap.utils.selector("#animation svg path");
-// signUp.addEventListener('click', () => {
-// gsap.fromTo("#an_svg #circle1_left", {
-// transformOrigin: '75% 40%',
-// rotate: 125,
-// });
-// })
-_gsap.gsap.to("#circle1_left", {
-  transformOrigin: "75% 40%",
-  rotate: 125,
-  duration: 1,
-  delay: 0.5
+const registerBtn = document.querySelector("#sign_up_btn");
+const logIn = document.querySelector("#sign_in_btn");
+// trigger on click
+registerBtn.addEventListener("click", () => {
+  // form animation section
+  _gsap.gsap.timeline().to("#sign__in", {
+    opacity: 0,
+    zIndex: 0,
+    duration: 0.35
+  }).to("#sign__in", {
+    display: "none",
+    duration: 0
+  }).to("#sign__up", {
+    opacity: 1,
+    zIndex: 500,
+    duration: 0.35,
+    display: 'block'
+  });
+  // animation section
+  timeline.to("#circle1_left", {
+    transformOrigin: "80% 35%",
+    rotate: 122,
+    x: -10 + "%",
+    y: 10 + "%",
+    fill: "#e63946",
+    duration: 1,
+    delay: 0.3,
+    animationPlayState: true
+  }).to("#circle1_right", {
+    rotate: 40,
+    x: 307,
+    y: -703.5,
+    fill: "#e63946"
+  }, "<").to("#circle2__left", {
+    rotate: 60,
+    fill: "#185ADB"
+  }, "<").to("#circle2__right", {
+    rotate: 213.5,
+    x: 378,
+    y: -358,
+    fill: "#185ADB"
+  }, "<").to("#portion_1", {
+    rotate: 140,
+    opacity: 0
+  }, "<").to("#portion_2", {
+    rotate: 52,
+    x: 178,
+    y: 420,
+    opacity: 0
+  }, "<").to("#portion_3", {
+    rotate: 43,
+    x: 558,
+    y: 645,
+    opacity: 0
+  }, "<").to("#portion_4", {
+    rotate: 62,
+    x: 430,
+    y: 100,
+    opacity: 0
+  }, "<").to("#first__circle", {
+    x: -265
+  }, "<").to("#secound__circle", {
+    x: -200,
+    y: 190
+  }, "<");
 });
-console.log(_gsap.gsap.utils.toArray("#first__circle half_circle"));
+// sign in animation
+logIn.addEventListener("click", () => {
+  // form animation
+  _gsap.gsap.timeline().to('#sign__up', {
+    opacity: 0,
+    zIndex: 0,
+    duration: 0.5
+  }).to("#sign__up", {
+    display: 'none',
+    duration: 0.1
+  }).to("#sign__in", {
+    opacity: 1,
+    zIndex: 500,
+    display: 'block',
+    duration: 0.5
+  });
+  // gsap timeline
+  timeline.reverse();
+});
 
-},{"gsap":"1iecp","../scss/style.scss":"188h8"}],"1iecp":[function(require,module,exports) {
+},{"../scss/style.scss":"188h8","gsap":"1iecp"}],"188h8":[function() {},{}],"1iecp":[function(require,module,exports) {
 var define;
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) : typeof define === 'function' && define.amd ? define(['exports'], factory) : (global = global || self, factory(global.window = global.window || ({})));
@@ -3899,6 +3975,6 @@ var define;
   }
 });
 
-},{}],"188h8":[function() {},{}]},["5Jifb","1oPsV"], "1oPsV", "parcelRequire1eea")
+},{}]},["5Jifb","1oPsV"], "1oPsV", "parcelRequire1eea")
 
 //# sourceMappingURL=index.371de241.js.map
