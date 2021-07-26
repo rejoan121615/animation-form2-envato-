b81278a758dafed14e1afebdf04503f6182cea13
Mohd Rejoan <wearecrx@gmail.com>
2021-07-26 16:54:37 +0600

    path id notworking on animation section

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 import '../scss/style.scss';
+import { gsap } from 'gsap';
 
 
 
@@ -9,24 +10,36 @@ getLink.forEach((item, index) => {
         link.preventDefault()
     })
 })
-// document.querySelectorAll('a').forEach((item, index) => {
-//     item.addEventListener('click', (link) => {
-//         link.preventDefault()
-//     })
-// })
-
-
-// sign in 
-const signIn = document.querySelector('#sign_in_btn');
-
-
-signIn.addEventListener('click', () => {
-    
+document.querySelectorAll('a').forEach((item, index) => {
+    item.addEventListener('click', (link) => {
+        link.preventDefault()
+    })
 })
 
 
+// sign in 
+const signUp = document.querySelector("#sign_up_btn");
+const targetAnimation = gsap.utils.selector("#animation svg path");
+
+// signUp.addEventListener('click', () => {
+//     gsap.fromTo("#an_svg #circle1_left", {
+//         transformOrigin: '75% 40%',
+//         rotate: 125,
+//     });
+// })
+
+gsap.to("#circle1_left", {
+    transformOrigin: "75% 40%",
+    rotate: 125,
+    duration: 1,
+    delay: 0.5
+});
+
+console.log(gsap.utils.toArray("#first__circle half_circle"));
+
+
 // sign up 
-const signUp = document.querySelector('#sign_up_btn');
+
 
 
 
